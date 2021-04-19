75787f9b22e1af9bda4f1fb17f3b986cbf33b742
Lexeria <avp-avp-13@mail.ru>
2021-04-19 23:34:19 +0500

    reset for front

--- app/index.js
+++ app/index.js
@@ -1,26 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import store from "./app/store"
-import { Provider } from "react-redux"
-
 const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-	ReactDOM.render(
-		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</React.StrictMode>,
-		document.getElementById("root")
-	)
+	res.send("Hello, world!")
 })
 
 app.all("*", (req, res) => {
 	res.sendStatus(404)
 })
-
 app.listen(process.env.PORT || 80)
