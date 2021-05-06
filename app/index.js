390c5d64ee6a1d285aef208b6138450980047115
Данил Панков <mistertimechip@mail.ru>
2021-05-06 19:25:34 +0500

    Added db initialization Semicolons ;)

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+const db = require("db");
+
+db.initialize();
 
 app.get("/", (req, res) => {
 	res.send("Hello, world!")
-})
+});
 
 app.all("*", (req, res) => {
 	res.sendStatus(404)
-})
+});
 
-app.listen(process.env.PORT || 80)
+app.listen(process.env.PORT || 80);
